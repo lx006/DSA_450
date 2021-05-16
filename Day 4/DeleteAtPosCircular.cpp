673eaf686884ab5c4f024eba0e7fe222cffd32ad
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-05-16 21:17:24 +0530

    Create DeleteAtPosCircular.cpp

---
+++ Day 4/DeleteAtPosCircular.cpp
@@ -0,0 +1,34 @@
+int deleteAtMid(int num)
+{
+	struct node *temp, *temp1;
+	int delnum;
+	if(head==NULL)
+	{
+		return 999;
+	}
+	else if(head->data==num)
+	{
+		temp = head;
+		head = temp->next;
+		delnum = temp->data;
+		free(temp);
+		return delnum;
+	}
+	else
+	{
+		temp = head;
+		while(temp->next->data!=num)
+		{
+			temp = temp->next;
+			if(temp==head)
+			{
+				return 888;
+			}
+		}
+		delnum=temp->next->data;
+		temp1=temp->next->next;
+		free(temp->next);
+		temp->next = temp1;
+		return delnum;
+	}
+}
