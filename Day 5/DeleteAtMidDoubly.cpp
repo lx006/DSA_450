51e470b37ca500601aba3612edb65af6a0bc0ef3
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-05-17 20:55:31 +0530

    Create DeleteAtMidDoubly.cpp

---
+++ Day 5/DeleteAtMidDoubly.cpp
@@ -0,0 +1,50 @@
+int deleteAtMid(int num)
+{
+	struct node *temp, *temp1;
+	int delnum;
+	if(head==NULL)
+	{
+		return 999;
+	}
+	else if(head->data==num)
+	{
+		temp = head;
+		head = temp->next;
+		head->prev=NULL;
+		delnum = temp->data;
+		free(temp);
+		return delnum;
+	}
+	else
+	{
+		temp = head;
+		while(temp->next->data!=num)
+		{
+			temp = temp->next;
+			if(temp==NULL)
+			{
+				return 888;
+			}
+		}
+		if(temp->next->next==NULL)
+		{
+			delnum=temp->next->data;
+			temp1 = temp->next->next;
+			free(temp->next);
+			temp->next=temp1;
+			return delnum;	
+		}
+		else
+		{
+			delnum=temp->next->data;
+			temp1 = temp->next->next;
+			free(temp->next);
+			temp->next=temp1;
+			temp1->prev=temp;
+			return delnum;
+		}
+		
+
+		
+	}
+}
