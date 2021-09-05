dca01146dd6e8f276373c21aa818720465ac4400
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-09-05 17:50:04 +0530

    Update KthSmallestElement.cpp

--- DSA_450_QuestionSolution/KthSmallestElement.cpp
+++ DSA_450_QuestionSolution/KthSmallestElement.cpp
@@ -7,4 +7,23 @@ public:
         return num[k-1];
         
     }
+    
+    //min-heap method
+   /* priority_queue<<int>,vector<int>,greater<int>> p ;
+    for(int i=0;i<len;i++)
+    {
+        p.push(num[i]);
+    }
+    int ans,i=1;
+    while(!p.empty())
+    {
+        if(i==k)
+        {
+            ans=i.top();
+            break;
+        }
+        i++;
+        p.pop();
+    }
+    cout<<ans<<endl;  */
 };
